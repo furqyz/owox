adf9c29cfacd6da1240be509d03e615b2a9aebb7
furqyz <76882861+furqyz@users.noreply.github.com>
2021-05-05 23:44:24 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const {
+	JsonDatabase
+} = require("wio.db");
+const db = new JsonDatabase("db");
+var kanal = "836559359428919297";
+var sahip = "778382988786401280";
+
+client.on("ready",()=>{
+	console.log("Hazır ! ");
+});
+
+//Furkan !rqyz
+
+setInterval(function(){
+	if(db.get("owodg") != "1"){
+		client.channels.get(kanal).send("owo h");
+	};
+},20000);
+
+setInterval(function(){
+	if(db.get("owodg") != "1"){
+		client.channels.get(kanal).send("wb");
+	};
+},27000);
+
+
+setInterval(function(){
+	if(db.get("owodg") != "1"){
+		client.channels.get(kanal).send("!rqyz GURURLA SUNAR!");
+	};
+},27000);
+
+//Furkan !rqyz
+
+client.on("message",message=>{
+	if(message.channel.id==kanal || message.channel.type == "dm"){
+		if(message.author.id=="408785106942164992"){
+			if(message.content.includes("Beep Boop") || message.content.includes("Please DM me") || message.content.includes("human") || message.content.includes("check")){
+				db.set("owodg","1");
+				message.channel.send("DURDURULDU! <@778382988786401280>");
+			};
+		};
+	};
+
+	if(message.author.id == sahip){
+		if(message.content == "!aç"){
+			db.set("owodg","0");
+			message.channel.send("Açıldı");
+		};
+	};
+});
+
+//Furkan !rqyz
+
+
+client.login("mfa.1Qj4asrQ8A8-r1ubiMYM9GtZFyopmiEjTFIr-9_-et6ZLytNm1XqfymroXqcNCgJeTO11lDUfTe7ACupBCqb");
